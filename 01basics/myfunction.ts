78a100974484cecab24742cf73feb6b59983381b
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-01-20 23:45:34 +0530

    refactor myfunction.ts to use var for login and add default parameter to signUp function

--- 01basics/myfunction.ts
+++ 01basics/myfunction.ts
@@ -6,12 +6,13 @@ function signup(name:string,email:string,isPaid:boolean):number{
     return 4
 }
 
+
 signup("ayo","ayo@gmail,com",true)
 
-let login = (name:string,email:string,isthere:boolean)=>{
+
+var login = (name:string,email:string,isthere:boolean)=>{
     console.log(name,email,isthere)
 }
-
 login("ayo","whas",true)
 
 // function getValue(myVal:number):string{ 
@@ -37,5 +38,10 @@ function consoleError(errmsg:string):void{
 }
 
 let userId = 12312
+
+
+let signUp = (name:string,email:string,isPaid:boolean = false)=>{}
+signUp("hello","hello") /// here we added the default value to it
+
 
 export{}
