c1b222660e7da834a568c01a56008e90608f075a
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-01-22 17:44:45 +0530

    refactor User class to implement private properties and add getter/setter methods for better encapsulation

---
+++ 02interm/gettersetter.ts
@@ -0,0 +1,35 @@
+class User5{
+
+    private _courseCount:number = 1
+
+    readonly city:string = "Jaipur"
+    constructor(
+        public email:string,
+        private name:string
+    ){
+        this.email = email
+        this.name = name
+    }
+
+    private deleteToken(){
+        console.log("Deleting token")
+    }
+    // getter
+    get AppleEmail():string{
+        return "apple"+this.email
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(count:number){ //setter cant have return type
+        if(count > 1){
+            throw new Error("Invalid course count")
+        }
+        this._courseCount = count;
+    }
+
+}
+
+// getteres and setters are designed in js to access and modify the private properties of a class
--- 02interm/publicprivate.ts
+++ 02interm/publicprivate.ts
@@ -1,6 +1,6 @@
 class User3{
     email:string
-    name:string
+    private name:string
     private readonly city:string = "Jaipur"
     constructor(email:string,name:string){
         this.email = email
@@ -12,3 +12,15 @@ class User3{
 const user3 = new User3 ("abc@gmail.com","Hitesh")
 console.log(user3.email) // Error: Property 'city' is private and only accessible within class 'User3'.
 // The private keyword restricts access to the city property to only the User class.
+class User4{
+    
+    readonly city:string = "Jaipur"
+    constructor( // another way to declare properties
+        public email:string,
+        public name:string
+    ){
+        this.email = email
+        this.name = name
+        this.city = "Delhi" // Error: Cannot assign to 'city' because it is a read-only property.
+    }
+}
